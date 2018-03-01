75d670ecd1f44beae264acf26626ce922f0c34ff
peterchauyw <peter@dhcp-10-248-126-41.eduroam.wireless.private.cam.ac.uk>
2018-03-01 13:23:17 +0000

    V2

--- app/src/main/java/com/example/peter/ble_watch/MainActivity.java
+++ app/src/main/java/com/example/peter/ble_watch/MainActivity.java
@@ -7,61 +7,31 @@ import android.os.Build;
 import android.os.Bundle;
 import android.support.wearable.activity.WearableActivity;
 import android.app.Activity;
-import android.app.ListActivity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.os.Bundle;
 import android.os.Environment;
 import android.os.Handler;
 import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
 import android.view.WindowManager;
-import android.widget.BaseAdapter;
-import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
-import java.util.ArrayList;
 import java.util.Objects;
-
-//public class MainActivity extends WearableActivity {
-//
-//    private TextView mTextView;
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-//
-//        mTextView = (TextView) findViewById(R.id.text);
-//
-//        // Enables Always-on
-//        setAmbientEnabled();
-//    }
-//}
 
 
 public class MainActivity extends WearableActivity {
-//    private LeDeviceListAdapter mLeDeviceListAdapter;
     private BluetoothAdapter mBluetoothAdapter;
     private boolean mScanning;
     private Handler mHandler;
-
     private static final int REQUEST_ENABLE_BT = 1;
     private static final int PERMISSION_REQUEST_COARSE_LOCATION = 1;
-    // Stops scanning after 10 seconds.
+    // Stops scanning after 10000 seconds.
     private static final long SCAN_PERIOD = 10000;
 
     @Override
@@ -86,61 +56,14 @@ public class MainActivity extends WearableActivity {
                 builder.show();
             }
         }
-
-
-
-
-//        getActionBar().setTitle(R.string.title_devices);
         mHandler = new Handler();
 
-        // Use this check to determine whether BLE is supported on the device.  Then you can
-        // selectively disable BLE-related features.
-//        if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
-//            Toast.makeText(this, R.string.ble_not_supported, Toast.LENGTH_SHORT).show();
-//            finish();
-//        }
         // Initializes a Bluetooth adapter.  For API level 18 and above, get a reference to
         // BluetoothAdapter through BluetoothManager.
         final BluetoothManager bluetoothManager =
                 (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
         mBluetoothAdapter = bluetoothManager.getAdapter();
-        // Checks if Bluetooth is supported on the device.
-//        if (mBluetoothAdapter == null) {
-//            Toast.makeText(this, R.string.error_bluetooth_not_supported, Toast.LENGTH_SHORT).show();
-//            finish();
-//            return;
-//        }
     }
-
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        getMenuInflater().inflate(R.menu.main, menu);
-//        if (!mScanning) {
-//            menu.findItem(R.id.menu_stop).setVisible(false);
-//            menu.findItem(R.id.menu_scan).setVisible(true);
-//            menu.findItem(R.id.menu_refresh).setActionView(null);
-//        } else {
-//            menu.findItem(R.id.menu_stop).setVisible(true);
-//            menu.findItem(R.id.menu_scan).setVisible(false);
-//            menu.findItem(R.id.menu_refresh).setActionView(
-//                    R.layout.actionbar_indeterminate_progress);
-//        }
-//        return true;
-//    }
-
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        switch (item.getItemId()) {
-//            case R.id.menu_scan:
-//                mLeDeviceListAdapter.clear();
-//                scanLeDevice(true);
-//                break;
-//            case R.id.menu_stop:
-//                scanLeDevice(false);
-//                break;
-//        }
-//        return true;
-//    }
 
     @Override
     public void onRequestPermissionsResult(int requestCode,
@@ -177,9 +100,6 @@ public class MainActivity extends WearableActivity {
             Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
             startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
         }
-        // Initializes list view adapter.
-//        mLeDeviceListAdapter = new LeDeviceListAdapter();
-//        setListAdapter(mLeDeviceListAdapter);
         scanLeDevice(true);
     }
 
@@ -197,20 +117,10 @@ public class MainActivity extends WearableActivity {
     protected void onPause() {
         super.onPause();
         scanLeDevice(false);
-//        mLeDeviceListAdapter.clear();
     }
 
     private void scanLeDevice(final boolean enable) {
         if (enable) {
-            // Stops scanning after a pre-defined scan period.
-//            mHandler.postDelayed(new Runnable() {
-//                @Override
-//                public void run() {
-//                    mScanning = false;
-//                    mBluetoothAdapter.stopLeScan(mLeScanCallback);
-//                    invalidateOptionsMenu();
-//                }
-//            }, SCAN_PERIOD);
             mScanning = true;
             mBluetoothAdapter.startLeScan(mLeScanCallback);
         } else {
@@ -220,91 +130,15 @@ public class MainActivity extends WearableActivity {
         invalidateOptionsMenu();
     }
 
-//    // Adapter for holding devices found through scanning.
-//    private class LeDeviceListAdapter extends BaseAdapter {
-//        private ArrayList<BluetoothDevice> mLeDevices;
-//        private LayoutInflater mInflator;
-//
-//        public LeDeviceListAdapter() {
-//            super();
-//            mLeDevices = new ArrayList<BluetoothDevice>();
-//            mInflator = MainActivity.this.getLayoutInflater();
-//        }
-//
-//        public void addDevice(BluetoothDevice device) {
-//            if (!mLeDevices.contains(device)) {
-//                mLeDevices.add(device);
-//            }
-//        }
-//
-//        public BluetoothDevice getDevice(int position) {
-//            return mLeDevices.get(position);
-//        }
-//
-//        public void clear() {
-//            mLeDevices.clear();
-//        }
-//
-//        @Override
-//        public int getCount() {
-//            return mLeDevices.size();
-//        }
-//
-//        @Override
-//        public Object getItem(int i) {
-//            return mLeDevices.get(i);
-//        }
-//
-//        @Override
-//        public long getItemId(int i) {
-//            return i;
-//        }
-//
-//        @Override
-//        public View getView(int i, View view, ViewGroup viewGroup) {
-//            ViewHolder viewHolder;
-//            // General ListView optimization code.
-//            if (view == null) {
-//                view = mInflator.inflate(R.layout.listitem_device, null);
-//                viewHolder = new ViewHolder();
-//                viewHolder.deviceAddress = (TextView) view.findViewById(R.id.device_address);
-//                viewHolder.deviceName = (TextView) view.findViewById(R.id.device_name);
-//                view.setTag(viewHolder);
-//            } else {
-//                viewHolder = (ViewHolder) view.getTag();
-//            }
-//
-//            BluetoothDevice device = mLeDevices.get(i);
-//            final String deviceName = device.getName();
-//            if (deviceName != null && deviceName.length() > 0)
-//                viewHolder.deviceName.setText(deviceName);
-//            else
-//                viewHolder.deviceName.setText(R.string.unknown_device);
-//            viewHolder.deviceAddress.setText(device.getAddress());
-//
-//            return view;
-//        }
-//    }
-
     // Device scan callback.
     private BluetoothAdapter.LeScanCallback mLeScanCallback =
             new BluetoothAdapter.LeScanCallback() {
-
-                //                @Override
-//                public void onLeScan(final BluetoothDevice device, int rssi, byte[] scanRecord) {
-//                    runOnUiThread(new Runnable() {
-//                        @Override
-//                        public void run() {
-//                            mLeDeviceListAdapter.addDevice(device);
-//                            mLeDeviceListAdapter.notifyDataSetChanged();
-//                        }
-//                    });
-//                }
                 @Override
                 public void onLeScan(final BluetoothDevice device, int rssi, byte[] scanRecord) {
                     Long tsLong = System.currentTimeMillis();
                     String ts = tsLong.toString();
-                    if (Objects.equals(device.getName(), new String("RBn"))) {
+//                    if (Objects.equals(device.getName(), new String("RBn"))) {
+                    if (Objects.equals(device.getAddress(), new String("30:52:CB:36:EF:44"))) {
                         writeToFile(Integer.toString(rssi) + "|" + ts + "\n");
 //                        runOnUiThread(new Runnable() {
 //                            @Override
@@ -358,10 +192,5 @@ public class MainActivity extends WearableActivity {
             Log.e("Exception", "File write failed: " + e.toString());
         }
     }
-
-//    static class ViewHolder {
-//        TextView deviceName;
-//        TextView deviceAddress;
-//    }
 }
 
